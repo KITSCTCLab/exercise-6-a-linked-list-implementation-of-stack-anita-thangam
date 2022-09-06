320d1914593cf8dd58f44834166b2db61cca330e
anita-thangam <110010649+anita-thangam@users.noreply.github.com>
2022-09-06 11:36:32 +0530

    anita

--- Main.py
+++ Main.py
@@ -9,19 +9,18 @@ class Stack:
     self.head = None
 
   def push(self, data) -> None:
-    # Write your code here
+    
 
   def pop(self) -> None:
-    # Write your code here
-
+   
   def status(self):
-    """
+   
     It prints all the elements of stack.
-    """
-    # Write your code here  
+   
+     
 
 
-# Do not change the following code
+
 stack = Stack()
 operations = []
 for specific_operation in input().split(','):
